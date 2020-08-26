a6bf5fb2dbd9ea16bc1fa6762d1b66fd7afdee0e
vRoma <Camomile1254>
2020-08-26 18:03:20 +0300

    Edited about section. Changed image size

--- assets/theme/php/elementor-extension/widgets/Elementor_About_Widget.php
+++ assets/theme/php/elementor-extension/widgets/Elementor_About_Widget.php
@@ -204,7 +204,7 @@ class Elementor_About_Widget extends \Elementor\Widget_Base {
         <div class="container">
 
                 <div class="row">
-                  <div class="image col-xl-7 d-flex align-items-stretch justify-content-center justify-content-lg-start">
+                  <div class="image col-xl-7 d-flex">
                     <img src="%3$s" alt="...">
                   </div>
                   <div class="col-xl-5 pl-0 pl-lg-5 pr-lg-1 d-flex align-items-stretch">
